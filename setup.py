361d37d440895d9391fbff89d98a22cb6069ce9f
NotKeKe <149957866+NotKeKe@users.noreply.github.com>
2026-05-15 22:11:11 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import re
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("scrapetube/__init__.py", encoding="utf-8") as f:
     version = re.findall(r"__version__ = \"(.+)\"", f.read())[0]
@@ -14,22 +14,27 @@ with open("requirements.txt", encoding="utf-8") as f:
 setup(
     name="scrapetube",
     version=version,
-    packages=["scrapetube"],
+    packages=find_packages(),
     include_package_data=True,
-    url="https://github.com/dermasmid/scrapetube",
+    url="https://github.com/NotKeKe/scrapetube",
     license="MIT",
     long_description=readme,
     long_description_content_type="text/markdown",
     author="Cheskel Twersky",
-    author_email="twerskycheskel@gmail.com",
+    # author_email="twerskycheskel@gmail.com",
     description="Scrape youtube without the official youtube api and without selenium.",
     keywords="youtube python channel videos search playlist list get",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    project_urls={"Documentation": "https://scrapetube.readthedocs.io/en/latest/"},
+    # classifiers=[
+    #     "Programming Language :: Python :: 3",
+    #     "License :: OSI Approved :: MIT License",
+    #     "Operating System :: OS Independent",
+    # ],
+    # project_urls={"Documentation": "https://scrapetube.readthedocs.io/en/latest/"},
     install_requires=requirements,
+
+    extras_require={
+        "async": ["httpx>=0.24.0"]  # 當使用者指定 [async] 時，才會額外多裝這些套件
+    },
+
     python_requires=">=3.6",
 )
