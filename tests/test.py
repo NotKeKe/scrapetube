a794cf06561bde100e875e8374968085a7e1fc1e
NotKeKe <149957866+NotKeKe@users.noreply.github.com>
2026-05-15 21:55:25 +0800

    scrapetube 現在被拆分至 scrapetube_sync

--- tests/test.py
+++ tests/test.py
@@ -5,7 +5,7 @@ sys.path.insert(
     0, "/".join(os.path.dirname(os.path.realpath(__file__)).split(os.sep)[:-1])
 )
 
-import scrapetube
+import scrapetube.scrapetube_sync as scrapetube
 
 
 videos = scrapetube.get_channel("UC9-y-6csu5WGm29I7JiwpnA", sort_by="popular")
